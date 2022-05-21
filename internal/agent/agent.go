361e633183ea6d268a8ca20027a5b4daacdffb77
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-21 16:42:58 +0430

    agent cpu status methods added

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -1,5 +1,33 @@
 package agent
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/load"
+)
+
+func getCpuInfo() {
+	cpuInfos, err := cpu.Info()
+	if err != nil {
+		fmt.Printf("get cpu info failed, err:%v", err)
+	}
+	for _, ci := range cpuInfos {
+		fmt.Println(ci)
+	}
+	//CPU utilization
+	for {
+		percent, _ := cpu.Percent(time.Second, false)
+		fmt.Printf("cpu percent:%v\n", percent)
+	}
+}
+
+func getCpuLoad() {
+	info, _ := load.Avg()
+	fmt.Printf("%v\n", info)
+}
+
 func Do() {
 
 }
