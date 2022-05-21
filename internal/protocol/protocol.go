1fcabef4e7f8dad6a0f55b05b40f8c87f51faca1
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-21 17:00:18 +0430

    protocol added

--- internal/protocol/protocol.go
+++ internal/protocol/protocol.go
@@ -1,5 +1,13 @@
 package protocol
 
 type Protocol struct {
+	// CPU
 	CPUUtilization float64 `json:"CPUUtilization"`
+	CPULoad        float64 `json:"CPULoad"`
+	// Memory
+	FreeMemory uint64 `json:"FreeMemory"`
+	UsedMemory uint64 `json:"UsedMemory"`
+	// Net
+	NetByteSend uint64 `json:"NetByteSend"`
+	NetByteRec  uint64 `json:"NetByteRec"`
 }
