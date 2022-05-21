38948be9572d5961fde8e3be9030e89cf37a9560
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-21 16:44:46 +0430

    memory and net status methods added

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -5,7 +5,10 @@ import (
 	"time"
 
 	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/load"
+	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/net"
 )
 
 func getCpuInfo() {
@@ -26,6 +29,38 @@ func getCpuInfo() {
 func getCpuLoad() {
 	info, _ := load.Avg()
 	fmt.Printf("%v\n", info)
+}
+
+// mem info
+func getMemInfo() {
+	memInfo, _ := mem.VirtualMemory()
+	fmt.Printf("mem info:%v\n", memInfo)
+}
+
+// disk info
+func getDiskInfo() {
+	parts, err := disk.Partitions(true)
+	if err != nil {
+		fmt.Printf("get Partitions failed, err:%v\n", err)
+		return
+	}
+	for _, part := range parts {
+		fmt.Printf("part:%v\n", part.String())
+		diskInfo, _ := disk.Usage(part.Mountpoint)
+		fmt.Printf("disk info:used:%v free:%v\n", diskInfo.UsedPercent, diskInfo.Free)
+	}
+
+	ioStat, _ := disk.IOCounters()
+	for k, v := range ioStat {
+		fmt.Printf("%v:%v\n", k, v)
+	}
+}
+
+func getNetInfo() {
+	info, _ := net.IOCounters(true)
+	for index, v := range info {
+		fmt.Printf("%v:%v send:%v recv:%v\n", index, v, v.BytesSent, v.BytesRecv)
+	}
 }
 
 func Do() {
