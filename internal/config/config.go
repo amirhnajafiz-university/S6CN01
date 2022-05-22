8b143e3ae570171c0d63f7fc703e604bb204ac38
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-22 23:16:06 +0430

    configs implemented

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,42 @@
 package config
 
+import (
+	"log"
+
+	"github.com/amirhnajafiz/packet-monitoring/internal/agent/socket"
+	"github.com/amirhnajafiz/packet-monitoring/internal/client"
+	"github.com/amirhnajafiz/packet-monitoring/internal/client/telemetry"
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/providers/structs"
+)
+
 type Config struct {
+	Agent     socket.Config
+	Client    client.Config
+	Telemetry telemetry.Config
 }
 
 func Load() Config {
-	return Default()
+	var instance Config
+
+	k := koanf.New(".")
+
+	// load default
+	if err := k.Load(structs.Provider(Default(), "koanf"), nil); err != nil {
+		log.Fatalf("error loading deafult: %v\n", err)
+	}
+
+	// load configs file
+	if err := k.Load(file.Provider("config.yaml"), yaml.Parser()); err != nil {
+		log.Fatalf("error loading config.yaml file: %v\n", err)
+	}
+
+	// unmarshalling
+	if err := k.Unmarshal("", &instance); err != nil {
+		log.Fatalf("error unmarshalling config: %v\n", err)
+	}
+
+	return instance
 }
--- internal/config/default.go
+++ internal/config/default.go
@@ -1,5 +1,26 @@
 package config
 
+import (
+	"github.com/amirhnajafiz/packet-monitoring/internal/agent/socket"
+	"github.com/amirhnajafiz/packet-monitoring/internal/client"
+	"github.com/amirhnajafiz/packet-monitoring/internal/client/telemetry"
+)
+
 func Default() Config {
-	return Config{}
+	return Config{
+		Agent: socket.Config{
+			ServerHost: "localhost",
+			ServerPort: "8080",
+			ServerType: "tcp",
+		},
+		Client: client.Config{
+			ServerHost: "localhost",
+			ServerPort: "8080",
+			ServerType: "tcp",
+		},
+		Telemetry: telemetry.Config{
+			Address: "1224",
+			Enabled: true,
+		},
+	}
 }
