94dbe2775050e5b252930780d39b7e37c73884c7
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-22 21:22:54 +0430

    metrics added

--- internal/telemetry/metric.go
+++ internal/telemetry/metric.go
@@ -17,3 +17,83 @@ type Metrics struct {
 	NetByteRec     prometheus.Histogram `json:"netByteRec,omitempty"`
 	Requests       prometheus.Counter   `json:"requests,omitempty"`
 }
+
+func newCounter(counterOpts prometheus.CounterOpts) prometheus.Counter {
+	ev := prometheus.NewCounter(counterOpts)
+
+	if err := prometheus.Register(ev); err != nil {
+		panic(err)
+	}
+
+	return ev
+}
+
+func newHistogram(histogramOpts prometheus.HistogramOpts) prometheus.Histogram {
+	ev := prometheus.NewHistogram(histogramOpts)
+
+	if err := prometheus.Register(ev); err != nil {
+		panic(err)
+	}
+
+	return ev
+}
+
+func NewMetrics() Metrics {
+	return Metrics{
+		Requests: newCounter(prometheus.CounterOpts{
+			Namespace:   Namespace,
+			Subsystem:   Subsystem,
+			Name:        "total_requests",
+			Help:        "total number of requests sent to prometheus client",
+			ConstLabels: nil,
+		}),
+		CPUUtilization: newHistogram(prometheus.HistogramOpts{
+			Namespace:   Namespace,
+			Subsystem:   Subsystem,
+			Name:        "cpu_utilization",
+			Help:        "average cpu utilization of system",
+			ConstLabels: nil,
+			Buckets:     prometheus.DefBuckets,
+		}),
+		CPULoad: newHistogram(prometheus.HistogramOpts{
+			Namespace:   Namespace,
+			Subsystem:   Subsystem,
+			Name:        "cpu_load",
+			Help:        "average cpu load of system",
+			ConstLabels: nil,
+			Buckets:     prometheus.DefBuckets,
+		}),
+		FreeMemory: newHistogram(prometheus.HistogramOpts{
+			Namespace:   Namespace,
+			Subsystem:   Subsystem,
+			Name:        "free_memory",
+			Help:        "free memory size",
+			ConstLabels: nil,
+			Buckets:     prometheus.DefBuckets,
+		}),
+		UsedMemory: newHistogram(prometheus.HistogramOpts{
+			Namespace:   Namespace,
+			Subsystem:   Subsystem,
+			Name:        "used_memory",
+			Help:        "in used memory size",
+			ConstLabels: nil,
+			Buckets:     prometheus.DefBuckets,
+		}),
+		NetByteSend: newHistogram(prometheus.HistogramOpts{
+			Namespace:   Namespace,
+			Subsystem:   Subsystem,
+			Name:        "net_byte_send",
+			Help:        "net bytes send",
+			ConstLabels: nil,
+			Buckets:     prometheus.DefBuckets,
+		}),
+		NetByteRec: newHistogram(prometheus.HistogramOpts{
+			Namespace:   Namespace,
+			Subsystem:   Subsystem,
+			Name:        "net_byte_rec",
+			Help:        "net bytes receive",
+			ConstLabels: nil,
+			Buckets:     prometheus.DefBuckets,
+		}),
+	}
+}
