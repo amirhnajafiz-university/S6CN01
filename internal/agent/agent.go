52f1768c043130f754f8e189a516bf46fed57091
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-22 20:27:47 +0430

    create status method added

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -1,5 +1,7 @@
 package agent
 
+import "github.com/amirhnajafiz/packet-monitoring/internal/protocol"
+
 type Agent struct {
 	Util Util
 }
@@ -10,8 +12,42 @@ func New() Agent {
 	}
 }
 
+func (a Agent) CreateStatus() (protocol.Protocol, error) {
+	var (
+		p   protocol.Protocol
+		err error
+	)
+
+	p.CPUUtilization, err = a.Util.cpuUtilization()
+	if err != nil {
+		return p, err
+	}
+
+	p.CPULoad, err = a.Util.cpuLoad()
+	if err != nil {
+		return p, err
+	}
+
+	p.FreeMemory, err = a.Util.freeMemory()
+	if err != nil {
+		return p, err
+	}
+
+	p.UsedMemory, err = a.Util.usedMemory()
+	if err != nil {
+		return p, err
+	}
+
+	p.NetByteSend, p.NetByteRec, err = a.Util.getNetInfo()
+	if err != nil {
+		return p, err
+	}
+
+	return p, nil
+}
+
 func (a Agent) Start() {
 	go func() {
-
+		// TODO
 	}()
 }
