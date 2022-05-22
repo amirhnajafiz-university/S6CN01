f2fca4a94d9673ef1d810b4f892fe30ddd7185eb
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-22 21:18:37 +0430

    telemetry client metrics added

--- internal/telemetry/metric.go
+++ internal/telemetry/metric.go
@@ -1,1 +1,19 @@
 package telemetry
+
+import "github.com/prometheus/client_golang/prometheus"
+
+const (
+	Namespace = "packet-monitoring"
+	Subsystem = "agents"
+)
+
+// Metrics has all the client metrics.
+type Metrics struct {
+	CPUUtilization prometheus.Histogram `json:"CPUUtilization,omitempty"`
+	CPULoad        prometheus.Histogram `json:"CPULoad,omitempty"`
+	FreeMemory     prometheus.Histogram `json:"freeMemory,omitempty"`
+	UsedMemory     prometheus.Histogram `json:"usedMemory,omitempty"`
+	NetByteSend    prometheus.Histogram `json:"netByteSend,omitempty"`
+	NetByteRec     prometheus.Histogram `json:"netByteRec,omitempty"`
+	Requests       prometheus.Counter   `json:"requests,omitempty"`
+}
